b42ce241971707ad79b406dbd5d353a61bc27709
Puleeno Nguyen <puleeno@gmail.com>
2026-01-13 09:23:33 +0700

    update style for search engine

--- src/SearchEngine.php
+++ src/SearchEngine.php
@@ -6,7 +6,7 @@ use Jankx\SearchEngine\Engines\TNTSearchEngine;
 
 class SearchEngine
 {
-    const VERSION = '1.0.1';
+    const VERSION = '1.0.2';
 
     protected static $instance;
     protected $driver;
