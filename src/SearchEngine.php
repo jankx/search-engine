3d2d45e52ba4ee2ce6c8b52af2defd54245b1a14
Puleeno Nguyen <puleeno@gmail.com>
2026-01-30 01:56:40 +0700

    Support query, filter from query params

--- src/SearchEngine.php
+++ src/SearchEngine.php
@@ -6,7 +6,7 @@ use Jankx\SearchEngine\Engines\TNTSearchEngine;
 
 class SearchEngine
 {
-    const VERSION = '1.0.17';
+    const VERSION = '1.0.18';
 
     protected static $instance;
     protected $driver;
--- src/UI/Components/Filter.php
+++ src/UI/Components/Filter.php
@@ -31,10 +31,22 @@ class Filter extends AbstractComponent
             ];
             $hasItems = true;
         }
+
+        $active_filters = isset($_GET['filter']) ? $_GET['filter'] : [];
+        if (empty($active_filters)) {
+            foreach ($enabledTax as $key => $value) {
+                $tax = str_replace('show_tax_', '', $key);
+                if (isset($_GET[$tax])) {
+                    $active_filters[$tax] = (array) $_GET[$tax];
+                }
+            }
+        }
+
         return $this->render_template('filters', [
             'tax_data' => $tax_data,
             'atts' => $atts,
             'has_items' => $hasItems,
+            'active_filters' => $active_filters,
         ]);
     }
 }
