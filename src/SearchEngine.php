473d6e51b9168b32d790f73f1f974b3dfaa93c05
Puleeno Nguyen <puleeno@gmail.com>
2026-02-05 07:52:57 +0700

    remove bold css

--- src/SearchEngine.php
+++ src/SearchEngine.php
@@ -6,7 +6,7 @@ use Jankx\SearchEngine\Engines\TNTSearchEngine;
 
 class SearchEngine
 {
-    const VERSION = '1.0.25';
+    const VERSION = '1.0.26';
 
     protected static $instance;
     protected $driver;
